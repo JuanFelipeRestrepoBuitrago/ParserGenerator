601cb6ed4eceef5188179e3ae09d03f608a2b660
Juan Felipe Restrepo Buitrago <jfrestrepb@eafit.edu.co>
2023-04-20 13:36:17 -0500

    Adding 3rd Grammar
    The third parser for a balanced parenthesis grammar was made

---
+++ src/Analizador3/Main.java
@@ -0,0 +1,31 @@
+package Analizador3;
+
+// Importing the generated files from the .bat file
+import Analizador3.Generado.Parser;
+import Analizador3.Generado.ParseException;
+import Analizador3.Generado.TokenMgrError;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class Main {
+    public static void main(String[] args) {
+        try {
+            // We create a new parser with the file to be analyzed
+            Parser parser = new Parser(new BufferedReader(new FileReader(".\\src\\Analizador3\\test.txt")));
+            // We analyze the file
+            parser.Verify();
+
+            System.out.println("Every string of the file is part of the grammar");
+        } catch (ParseException e) {
+            System.err.println(e.getMessage());
+        } catch (FileNotFoundException e) {
+            Logger.getLogger(Analizador1.Main.class.getName()).log(Level.SEVERE, "Error al intentar leer el archivo.", e);
+        } catch(TokenMgrError e){
+            System.err.println(e.getMessage());
+        }
+    }
+}
---
+++ src/Analizador3/compilarGramatica.bat
@@ -0,0 +1,2 @@
+java -cp "D:\Archivos de Aplicaciones\IntellJ IDEA\PROJECTS\EAFIT\Lenguajes Formales y Compiladores\ProyectoJavaCC\javacc.jar" javacc Grammar.jj
+pause
