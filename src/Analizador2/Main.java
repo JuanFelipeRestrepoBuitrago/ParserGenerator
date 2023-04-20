97a8d0b435be2cfb721a9d7c24a3265360aec23a
Juan Felipe Restrepo Buitrago <jfrestrepb@eafit.edu.co>
2023-04-20 09:32:02 -0500

    First Two Grammars
    This commit is the first of the project, it adds the first two context-free grammars with their own parsers

---
+++ src/Analizador2/Main.java
@@ -0,0 +1,23 @@
+package Analizador2;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class Main {
+    public static void main(String[] args) {
+        try {
+            Gramatica parser = new Gramatica(new BufferedReader(new FileReader(".\\src\\Analizador2\\prueba2.txt")));
+            parser.Analizar();
+            System.out.println("Todas las sentencias son correctas.");
+        } catch (ParseException e) {
+            System.err.println(e.getMessage());
+        } catch (FileNotFoundException e) {
+            Logger.getLogger(Analizador1.Main.class.getName()).log(Level.SEVERE, "Error al intentar leer el archivo.", e);
+        } catch(TokenMgrError e){
+            System.err.println(e.getMessage());
+        }
+    }
+}
